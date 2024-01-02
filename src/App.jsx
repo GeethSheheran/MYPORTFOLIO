42612cb63bb2d45c81337cd21e279f2895ef51f7
geethsheheran <gs.sheheran@gmail.com>
2024-01-02 12:11:28 +0530

    commit message

--- src/App.jsx
+++ src/App.jsx
@@ -13,10 +13,6 @@ import { useEffect } from "react";
 import blogPosts from "./content/blogPosts";
 import ContactForm from "./components/ContactForm";
 import BlogPost from "./components/BlogPost";
-// import { Switch, Router, Route } from 'react-router-dom';
-// import UniProject from "./components/UniProject.jsx";
-import { BrowserRouter, Routes , Route } from "react-router-dom";
-import Sport from "./components/Sport";
 
 function App() {
   const controls = useAnimation();
@@ -44,113 +40,69 @@ function App() {
 
   return (
     <>
-
-    {/* <div>
-      <BrowserRouter>
-        <Routes>
-          <Route index element = {<App/>}/>
-          <Route path="/Sport" element={<Sport/>}/>
-        </Routes>
-      </BrowserRouter>
-    </div> */}
-
-
       <Navbar
         firstName={information.userData.firstName}
         lastName={information.userData.lastName}
       />
-      <Hero
-        img={information.userData.img}
-        description={information.userData.description}
-        title={information.userData.title}
-      />
-      <div className="hr"></div>
-      <br/>
-<br id="services"/>
+      <video className="background-video" autoPlay loop muted>
+        <source src="./rec.mp4" type="video/mp4" />
+        {/* Add additional video sources for cross-browser support */}
+      </video>
       
-      <section>
-        <Heading firstWord="my" secondWord="Services" />
-<br/>
-        <Services/>
-      </section>
-
-<br/>
-<br id="projects"/>
-<br/>
-<br/>
-
-      <div className="hr"></div>
-      <section>
-        <Heading firstWord="my" secondWord="Projects" />
-        <motion.div
-          className="project-map"
-          ref={ref}
-          initial="hidden"
-          animate={controls}
-          variants={containerVariants}
-        >
-          {projects.map((project, index) => (
-            <div key={index}>
-              <ProjectCard
-                name={project.name}
-                img={project.img}
-                description={project.description}
-                source={project.sourceCode}
-                preview={project.preview}
+      <div className="content-container">
+        <Hero
+          img={information.userData.img}
+          description={information.userData.description}
+          title={information.userData.title}
+        />
+        <div className="hr"></div>
+        <section id="services">
+          <Heading firstWord="my" secondWord="Services" />
+          <Services />
+        </section>
+        <div className="hr"></div>
+        <section id="projects">
+          <Heading firstWord="my" secondWord="Projects" />
+          <motion.div
+            className="project-map"
+            ref={ref}
+            initial="hidden"
+            animate={controls}
+            variants={containerVariants}
+          >
+            {projects.map((project, index) => (
+              <div key={index}>
+                <ProjectCard
+                  name={project.name}
+                  img={project.img}
+                  description={project.description}
+                  source={project.sourceCode}
+                  preview={project.preview}
+                />
+              </div>
+            ))}
+          </motion.div>
+        </section>
+        <section id="blog">
+          <Heading firstWord="ex" secondWord="Activities" />
+          <div className="posts">
+            {blogPosts.map((post, index) => (
+              <BlogPost
+                key={index}
+                title={post.title}
+                image={post.image}
+                read={post.link}
+                date={post.pubDate}
               />
-            </div>
-          ))}
-        </motion.div>
-      </section>
-      
-      <br id="blog"/>
-      <br/>
-      <br/>
-      <br/>
-      <section>
-        <Heading firstWord="ex" secondWord="Activities" />
-        <div className="posts">
-          {blogPosts.map((post, index) => (
-            <BlogPost
-              key={index}
-              title={post.title}
-              image={post.image}
-              read={post.link}
-              date={post.pubDate}
-            />
-          ))}
-        </div>
-        {/* <a className="cyber-scourge" href="https://blog.randiltharusha.me">
-          View More Posts
-        </a> */}
-      </section>
-      <br/>
-      <br id="skills"/>
-      <br/>
-      <br/>
-      {/* <section id="">
-        <Heading firstWord="my" secondWord="Services" />
-        <motion.div
-          className="skill-map"
-          ref={ref}
-          initial="hidden"
-          animate={controls}
-          variants={containerVariants}
-        >
-          {skills.map((skill, index) => (
-            <motion.div key={index} variants={skillVariants}>
-              <Skill skill={skill} />
-            </motion.div>
-          ))}
-        </motion.div>
-      </section> */}
-      
-      <section id="contact">
-        <Heading firstWord="Contact" secondWord="Me" />
-        <ContactForm />
-      </section>
-
-      <Footer />
+            ))}
+          </div>
+        </section>
+        <section id="contact">
+          <Heading firstWord="Contact" secondWord="Me" />
+          <ContactForm />
+        </section>
+        <Footer />
+      </div>
     </>
   );
 }
